db3ca42bfcfc17afcbe385ba4c031e0ad9c93639
29099 <Datnp5@gmail.com>
2022-12-01 03:32:31 +0700

    init dev

---
+++ aspnet-core/src/XiusEcom.Admin.Application.Contracts/Permissions/XiusEcomPermissions.cs
@@ -0,0 +1,9 @@
+﻿namespace XiusEcom.Permissions;
+
+public static class XiusEcomPermissions
+{
+    public const string GroupName = "XiusEcom";
+
+    //Add your own permission names. Example:
+    //public const string MyPermission1 = GroupName + ".MyPermission1";
+}
---
+++ aspnet-core/test/XiusEcom.Admin.Application.Tests/XiusEcomApplicationTestBase.cs
@@ -0,0 +1,6 @@
+﻿namespace XiusEcom;
+
+public abstract class XiusEcomApplicationTestBase : XiusEcomTestBase<XiusEcomApplicationTestModule>
+{
+
+}
