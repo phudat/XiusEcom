1001cbd5cfaf33291615b909c341fe7347e44335
Datnp5 <Datnp5@fptshop.com.vn>
2022-12-06 17:31:57 +0700

    add entity

---
+++ aspnet-core/src/XiusEcom.Domain/Products/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XiusEcom.Products
+{
+    public class Product : Volo.Abp.Domain.Entities.Auditing.CreationAuditedAggregateRoot<Guid>
+    {
+        public Guid ManufactureId { get; set; }
+        public string Code { get; set; }
+        public string SKU { get; set; }
+        public string SortOrder { get; set; }
+        public bool Visibility { get; set; }
+        public bool IsActive { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
---
+++ aspnet-core/src/XiusEcom.EntityFrameworkCore/Migrations/20221206055416_changedb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XiusEcom.Migrations
+{
+    public partial class changedb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
